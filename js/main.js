a85bafb34eed4a2e4e4d63e8fb31c2c7c85a6aa6
Serg Ushakov <sergushakov.public@gmail.com>
2015-07-16 18:40:20 +0300

    completed new point form

--- js/main.js
+++ js/main.js
@@ -1,28 +1,29 @@
-
-
-
 /* global window */
 (function(w){
     'use strict';
-	
     w.angular.module('myMap',['ui.sortable'])
-		.config(function($interpolateProvider) {
-			$interpolateProvider.startSymbol('{!');
-			$interpolateProvider.endSymbol('!}');
-		})
-        .controller('mapC',function($scope){
+        .controller('mapC',function($scope,$compile,$http){
             var scope = $scope;
             scope.map = w.L.map('map').setView([55.751244, 37.618423], 15);
             w.L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(scope.map);
             scope.map.on('click',function(e){
-                scope.popup = w.L.popup()
-                    .setLatLng(e.latlng)
-                    .setContent(scope.newPointForm())
-                    .openOn(scope.map);
-                w.$("input[type=text]").focus();
+                // scope.inProgress = true;
+                $http.get('tpl/form.html').success(function(x){
+                    var point = new Point(e.latlng);
+                    scope.popup = w.L.popup({ maxWidth:500, minWidth: 500, autoPan: true, keepInView: false})
+                        .setLatLng(e.latlng)
+                        .setContent($compile(w.angular.element(x))(point)[0])
+                        .openOn(scope.map);
+                    w.$(".fake").on("change",handleFiles);
+                    w.$("form").on("submit",point.save);
+                    w.$("input[type=text]").focus();
+                });
+
             });
+            scope.categories = ["Museum","Hotel","Zoo","Gallery","Other"];
+            scope.currency = ["USD","EUR","RUB"];
             scope.newPointForm = function(){
                 var root = w.$("<form/>",{submit:function(e){
                     e.preventDefault();
@@ -32,41 +33,59 @@
                 w.$("<input/>",{class: "btn btn-success btn-sm",type:"submit",value:"Добавить к маршруту"}).appendTo(root);
                 return root[0];
             };
-
-            function Point(title){
-                if(!title) return false;
-                var point = this;
-                this.title = title;
-                this.order = scope.points.length;
-                this.coord = scope.popup.getLatLng();
-                this.toString = function(){
-                    return [this.order,this.title,this.coord.lat,this.coord.lng].join(" | ");
+            function Point(coord){
+                var local = scope.$new();
+                local.order = scope.points.length;
+                local.coord = coord;
+                local.showExtra = false;
+                local.currency = "USD";
+                local.images = [];
+                local.country = "loaded...";
+                local.city = "loaded...";
+                local.region = "loaded...";
+                local.toJSON = function(){
+                    return { order: local.order, title: local.title, lat: local.coord.lat, lng: local.coord.lng };
                 };
-                this.toJSON = function(){
-                    return { order: point.order, title: point.title, lat: point.coord.lat, lng: point.coord.lng };
+                local.toString = function(){
+                    return [local.order,local.title,local.coord.lat,local.coord.lng].join(" ");
                 };
-                this.newRemoveForm = function(){
+                local.newRemoveForm = function(){
                     var root = w.$("<div/>");
-                    w.$("<h3/>").html(point.title).appendTo(root);
+                    w.$("<h3/>").html(local.title).appendTo(root);
                     w.$("<button/>",
                         { class:"btn btn-danger",
                           click: function(){
-                              scope.points.splice(scope.points.indexOf(point),1);
-                              scope.map.removeLayer(point.marker);
+                              scope.points.splice(scope.points.indexOf(local),1);
+                              scope.map.removeLayer(local.marker);
                               scope.$apply();
                               scope.map.closePopup();
                           }
                         }).html("Убрать из маршрута").appendTo(root);
                     return root[0];
                 };
-                scope.points.push(this);
-                this.marker = w.L.marker(scope.popup.getLatLng()).addTo(scope.map);
-                this.marker.bindPopup(this.newRemoveForm()).openPopup();
-                scope.map.closePopup();
-                scope.$apply();
-				console.log(this);
-                return this;
+                local.save = function(ev){
+                    console.log(w.JSON.stringify($("form").serializeArray()));
+                    scope.points.push(local);
+                    local.marker = w.L.marker(scope.popup.getLatLng()).addTo(scope.map);
+                    local.marker.bindPopup(local.newRemoveForm()).openPopup();
+                    scope.map.closePopup();
+                    scope.$apply();
+                    $(ev.target).ajaxSubmit({success:function(x){console.log(x);},error:function(x){console.log("please, set correct form action url");}});
+                    return false;
+                };
+                $http.get("https://nominatim.openstreetmap.org/reverse?format=json&lat="+local.coord.lat+"&lon="+local.coord.lng+"&zoom=10&addressdetails=1")
+                    .success(function(rsp){
+                        local.country = rsp.address.country;
+                        local.city = rsp.address.state;
+                        local.region = rsp.address.city;
+                    })
+                    .error(function(err){
+                        return false;
+                    });
+                ;
+                return local;
             }
+            
             scope.points = [];
             scope.$watchCollection('points',function(v){
                 w.console.log(w.JSON.stringify(v.map(function(x){ return x.toJSON(); })));
@@ -77,3 +96,19 @@
         })
     ;
 })(window);
+
+function handleFiles(ev){
+    if(ev.target.files.length > 0){
+        Array.prototype.forEach.call(ev.target.files,function(f){
+            var v = new FileReader();
+            v.onload = function(d){
+                $("<img/>",{src:d.target.result}).appendTo(".images");
+            };
+            v.onerror = function(e){
+                return false;
+            };
+            v.readAsDataURL(f);
+        });
+    }
+    return false;
+};
